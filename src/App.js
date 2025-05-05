9718aa28875fd841de318112536f88a6f3286c82
MariamShamia2002 <mariamshamia3@gmail.com>
2025-05-05 14:21:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setOpen] = useState(true);
+  console.log(isOpen);
+  function handlPrevious() {
+    if (step > 1) setStep((step) => step - 1);
+  }
+  function handlNext() {
+    if (step < 3) setStep((step) => step + 1);
+  }
+  return (
+    <>
+      <button className="close" onClick={() => setOpen((s) => !s)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            {step} : {messages[step - 1]}{" "}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={handlPrevious}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handlNext}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
